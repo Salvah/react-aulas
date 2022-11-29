6684ac80a700089abc510ec395aa211b567649fb
Thyago Salva <thyago.salva@bento.local>
2022-11-29 14:59:02 -0300

    axios

--- src/components/Exemplo.js
+++ src/components/Exemplo.js
@@ -1,6 +1,8 @@
+import axios from 'axios'
 import React, {useEffect, useState} from 'react'
 
 export const Exemplo = () => {
+    const [loading, setLoading] = useState(true)
     const [texto, setTexto] = useState("")
     const [itens, setItens] = useState([])
 
@@ -8,7 +10,6 @@ export const Exemplo = () => {
     
     const handleChange = ({target}) => setTexto(target.value)
     
-
     const remove = item => setItens(
         itens.filter(i => i !== item)
     )
@@ -19,7 +20,20 @@ export const Exemplo = () => {
     
     }
 
+    useEffect(() => {
+        const load = async () => {
+            const r = await axios.get(
+                'http://localhost/backend/lista.php')
 
+            setItens(r.data)
+            
+            setLoading(false)
+        }
+
+        load()
+    }, [])
+
+    console.log(loading)
     return (
         <div>
             <h2>Lista de Compras</h2>
@@ -27,12 +41,13 @@ export const Exemplo = () => {
                 <input name="texto" value={texto} onChange={handleChange} type="text"/>
                 <button disabled={disabled} onClick={add} type="button">+</button>
             
-            <ul>
+            {loading && <p>Aguarde...</p>}
+            {!loading && (<ul>
                 {itens.map(item => <li key={item.id}>
                     <span>{item.nome}</span>
                     <button onClick={()=>remove(item.id)} type="button">-</button>
                 </li>)}
-            </ul>
+            </ul>)}
             
         </div>
     )
